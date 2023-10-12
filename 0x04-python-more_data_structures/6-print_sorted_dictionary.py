83e28e93465ba39a3ebe44b6028d559263c381a4
souad <arziki.souad@gmail.com>
2023-10-11 18:46:13 -0700

    function that prints a dictionary by ordered keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for n, m in sorted(a_dictionary.items()):
-        print("{}: {}".format(n, m))
+    if a_dictionary is not None:
+        for n, m in sorted(a_dictionary.items()):
+            print("{}: {}".format(n, m))
