666047d7b3af4dffe09ac963422aa9e03408a8d7
souad <arziki.souad@gmail.com>
2023-11-23 10:24:22 -0800

    the readme file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """function that add tow integers"""
+    if a is not an integer:
+        raise TypeError("a must be an integer")
+    if b is not an integer:
+        raise TypeError("b must be an integer")
